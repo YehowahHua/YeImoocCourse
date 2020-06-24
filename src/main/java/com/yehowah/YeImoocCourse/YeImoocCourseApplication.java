4cb88df89340623cde3eecb5c7ee5984a5b15dc2
YehowahHua <chen1124hua@foxmail.com>
2020-06-24 15:02:03 +0800

    Initial commit

---
+++ src/main/java/com/yehowah/YeImoocCourse/YeImoocCourseApplication.java
@@ -0,0 +1,13 @@
+package com.yehowah.YeImoocCourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YeImoocCourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YeImoocCourseApplication.class, args);
+	}
+
+}
